999496aa97945401e5dc910adc266c377558e865
Ben Gonzales <bgonzales20001@gmail.com>
2020-04-02 18:38:40 -0500

    Update git-blender-repo.sh

--- git-blender-repo.sh
+++ git-blender-repo.sh
@@ -19,11 +19,13 @@ git submodule foreach git pull --rebase origin master
 
 sudo apt-get -y install cmake-curses-gui
 sudo apt-get -y install libtbb-dev
-
-#sudo apt-get install -y libjpeg-dev libopenexr-dev libopenjpeg-dev libtiff-dev libopenal-dev libsdl2-dev libsdl-dev libsndfile1-dev libfftw3-dev  libssl-dev libboost-all-dev libpython-dev libtbb-dev libopenexr-dev libsdl1.2-dev libopenjp2-7-dev opencollada-dev libjemalloc-dev libopenvdb-dev libblosc-dev libspnav0 alembic python-alembic python3-alembic libopenimageio-dev llvm libopencolorio-dev libvpx5 libopenimageio-dev python3-numpy openexr libjxr-tools
-#sudo apt-get install libjpeg-dev libopenexr-dev libopenjpeg-dev libtiff-dev libopenal-dev libsdl2-dev libsdl-dev libsndfile1-dev libfftw3-dev  libssl-dev libboost-all-dev libpython-dev libtbb-dev -y libopenexr-dev libsdl1.2-dev libopenjp2-7-dev opencollada-dev libjemalloc-dev libopenvdb-dev libblosc-dev libspnav0 alembic python-alembic python3-alembic libopenimageio-dev llvm libopencolorio-dev libvpx5 libopenimageio-dev python3-numpy openexr libjxr-tools  libjack-jackd2-dev
-#sudo apt-get install libjpeg-dev libopenexr-dev libopenjpeg-dev libtiff-dev libopenal-dev libsdl2-dev libsdl-dev libsndfile1-dev libfftw3-dev  libssl-dev libboost-all-dev libpython-dev libtbb-dev -y libopenexr-dev libsdl1.2-dev libopenjp2-7-dev opencollada-dev libjemalloc-dev libopenvdb-dev libblosc-dev libspnav0 alembic python-alembic python3-alembic libopenimageio-dev llvm libopencolorio-dev libvpx5 libopenimageio-dev python3-numpy openexr libjxr-tools  libjack-jackd2-dev libavformat-dev libopenimageio-dev libopenimageio2.0 python3-openimageio libswscale-dev libavdevice-dev
-sudo apt-get install libjpeg-dev libopenexr-dev libtiff-dev libopenal-dev libsdl2-dev libsdl-dev libsndfile1-dev libfftw3-dev  libssl-dev libboost-all-dev libpython-dev libtbb-dev -y libopenexr-dev libsdl1.2-dev libopenjp2-7-dev opencollada-dev libjemalloc-dev libopenvdb-dev libblosc-dev libspnav0 alembic python-alembic python3-alembic llvm libopencolorio-dev libvpx5 libopenimageio-dev python3-numpy openexr libjxr-tools  libjack-jackd2-dev libavformat-dev libopenimageio-dev libopenimageio2.0 python3-openimageio libswscale-dev libavdevice-dev libopenjp2-7
+sudo apt-get install -y libjpeg-dev libopenexr-dev libtiff-dev libopenal-dev libsdl2-dev libsdl-dev 
+sudo apt-get install -y libsndfile1-dev libfftw3-dev  libssl-dev libboost-all-dev libpython-dev libtbb-dev 
+sudo apt-get install -y libopenexr-dev libsdl1.2-dev libopenjp2-7-dev opencollada-dev libjemalloc-dev 
+sudo apt-get install -y libopenvdb-dev libblosc-dev libspnav0 alembic python-alembic python3-alembic 
+sudo apt-get install -y llvm libopencolorio-dev libvpx5 libopenimageio-dev python3-numpy openexr 
+sudo apt-get install -y libjxr-tools  libjack-jackd2-dev libavformat-dev libopenimageio-dev 
+sudo apt-get install -y libopenimageio2.0 python3-openimageio libswscale-dev libavdevice-dev libopenjp2-7
 sudo apt-get install -y openimageio-tools
 
 sudo apt-get install cmake -y
