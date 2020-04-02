194962bdb7a909ee7587c4d7fe2f1ad767c36dbc
Ben Gonzales <bgonzales20001@gmail.com>
2020-04-02 14:02:57 -0500

    Update docker-compose-install.sh

--- docker-compose-install.sh
+++ docker-compose-install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+#
+# https://dev.to/rohansawant/installing-docker-and-docker-compose-on-the-raspberry-pi-in-5-simple-steps-3mgl
+#
 curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker pi
 #docker run hello-world
