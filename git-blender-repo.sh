131f6ecde19ba8bb39dcc753ab10368727efaf31
Ben Gonzales <bgonzales20001@gmail.com>
2020-04-02 16:43:35 -0500

    Update git-blender-repo.sh

--- git-blender-repo.sh
+++ git-blender-repo.sh
@@ -17,8 +17,8 @@ git submodule foreach git checkout master
 git submodule foreach git pull --rebase origin master
 
 
-sudo apt-get install cmake-curses-gui
-sudo apt-get install libtbb-dev
+sudo apt-get -y install cmake-curses-gui
+sudo apt-get -y install libtbb-dev
 
 #sudo apt-get install -y libjpeg-dev libopenexr-dev libopenjpeg-dev libtiff-dev libopenal-dev libsdl2-dev libsdl-dev libsndfile1-dev libfftw3-dev  libssl-dev libboost-all-dev libpython-dev libtbb-dev libopenexr-dev libsdl1.2-dev libopenjp2-7-dev opencollada-dev libjemalloc-dev libopenvdb-dev libblosc-dev libspnav0 alembic python-alembic python3-alembic libopenimageio-dev llvm libopencolorio-dev libvpx5 libopenimageio-dev python3-numpy openexr libjxr-tools
 sudo apt-get install libjpeg-dev libopenexr-dev libopenjpeg-dev libtiff-dev libopenal-dev libsdl2-dev libsdl-dev libsndfile1-dev libfftw3-dev  libssl-dev libboost-all-dev libpython-dev libtbb-dev -y libopenexr-dev libsdl1.2-dev libopenjp2-7-dev opencollada-dev libjemalloc-dev libopenvdb-dev libblosc-dev libspnav0 alembic python-alembic python3-alembic libopenimageio-dev llvm libopencolorio-dev libvpx5 libopenimageio-dev python3-numpy openexr libjxr-tools  libjack-jackd2-dev
