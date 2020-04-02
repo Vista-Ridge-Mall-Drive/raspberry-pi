ff16149ffe869bf3728bb2fdf543a4de83f864fa
Ben Gonzales <bgonzales20001@gmail.com>
2020-04-02 15:06:52 -0500

    Update git-blender-repo.sh

--- git-blender-repo.sh
+++ git-blender-repo.sh
@@ -14,3 +14,7 @@ cd blender
 git submodule update --init --recursive
 git submodule foreach git checkout master
 git submodule foreach git pull --rebase origin master
+
+sudo apt-get install cmake -y
+make
+
