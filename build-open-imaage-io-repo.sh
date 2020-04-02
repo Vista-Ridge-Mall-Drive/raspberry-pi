f070005fc476b4aa4883c421a1f93e980a39c4ec
Ben Gonzales <bgonzales20001@gmail.com>
2020-04-02 18:27:36 -0500

    Update build-open-imaage-io-repo.sh

--- build-open-imaage-io-repo.sh
+++ build-open-imaage-io-repo.sh
@@ -7,7 +7,10 @@ cd oiio
 mkdir build
 cd build
 
-sudo apt-get install -y cmake libtiff-dev libopenexr-dev libboost-all-dev
+
+sudo apt-get install -y cmake libtiff-dev libopenexr-dev libboost-all-dev libbz2-dev libhdf5-dev libopencolorio-dev libopencv-calib3d-dev libdcmtk-dev field3d-tools 
+
+sudo apt-get install -y libgif-dev libheif-dev libraw-dev libopenvdb-dev webp libqt4-opengl-dev pybind11-dev libsquish-dev
 
 cmake .. -DCMAKE_BUILD_TYPE=Release
 
