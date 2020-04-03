48d892a86c7f1b63a5c21f24e816b27af13bed5a
Ben Gonzales <bgonzales20001@gmail.com>
2020-04-02 22:26:22 -0400

    Update git-blender-repo.sh

--- git-blender-repo.sh
+++ git-blender-repo.sh
@@ -22,8 +22,8 @@ sudo apt-get -y install libtbb-dev
 sudo apt-get install -y libjpeg-dev libopenexr-dev libtiff-dev libopenal-dev libsdl2-dev libsdl-dev 
 sudo apt-get install -y libsndfile1-dev libfftw3-dev  libssl-dev libboost-all-dev libpython-dev libtbb-dev 
 sudo apt-get install -y libopenexr-dev libsdl1.2-dev libopenjp2-7-dev opencollada-dev libjemalloc-dev 
-sudo apt-get install -y libopenvdb-dev libblosc-dev libspnav0 alembic python-alembic python3-alembic 
-sudo apt-get install -y llvm libopencolorio-dev libvpx5 libopenimageio-dev python3-numpy openexr 
+sudo apt-get install -y libopenvdb-dev libblosc-dev libspnav-dev alembic python-alembic python3-alembic 
+sudo apt-get install -y llvm-7-dev libopencolorio-dev libvpx5 libopenimageio-dev python3-numpy libopenexr-dev 
 sudo apt-get install -y libjxr-tools  libjack-jackd2-dev libavformat-dev libopenimageio-dev 
 sudo apt-get install -y libopenimageio2.0 python3-openimageio libswscale-dev libavdevice-dev libopenjp2-7
 sudo apt-get install -y openimageio-tools
